a6a57b9ae78c4e628ea9166abd4aea9d962e29ce
CalebeSG <calebeguimaraes10@gmail.com>
2022-06-30 15:59:35 -0300

    config setup

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        orange: {
+          100: "hsl(25, 100%, 94%)",
+          500: "hsl(26, 100%, 55%)",
+        },
+        grayish_blue: {
+          100: "hsl(223, 64%, 98%)",
+          400: "hsl(220, 14%, 75%)",
+          500: "hsl(219, 9%, 45%)",
+          800: "hsl(220, 13%, 13%)",
+        },
+      },
+      fontFamily: {
+        kumbh: ["Kumbh Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
