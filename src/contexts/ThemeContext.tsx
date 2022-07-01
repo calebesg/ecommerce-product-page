c2e974b75d265c0ce4f6d485e6ced03afe0b07ec
CalebeSG <calebeguimaraes10@gmail.com>
2022-07-01 13:20:34 -0300

    create theme context

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,26 @@
+import { createContext, useState } from "react";
+
+const Context = createContext({
+  theme: "light",
+  changeTheme: () => {},
+});
+
+interface ThemeProviderProps {
+  children: any;
+}
+
+export function ThemeProvider({ children }: ThemeProviderProps) {
+  const [theme, setTheme] = useState("light");
+
+  const changeTheme = function () {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <Context.Provider value={{ theme: theme, changeTheme: changeTheme }}>
+      {children}
+    </Context.Provider>
+  );
+}
+
+export default Context;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
+import ThemeContext from "../contexts/ThemeContext";
+
 export default function Home() {
   return (
     <div>
       <h1>Hello</h1>
+      <ThemeContext.Consumer>
+        {(value) => (
+          <button onClick={() => value.changeTheme()}>Dark Mode</button>
+        )}
+      </ThemeContext.Consumer>
     </div>
   );
 }
