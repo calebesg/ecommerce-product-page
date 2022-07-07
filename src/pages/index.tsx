cb6cca9acd7856e78f3febff7f80f41f7f7f70ca
CalebeSG <calebeguimaraes10@gmail.com>
2022-07-07 13:37:04 -0300

    create description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,34 +54,40 @@ export default function Home() {
 
         <div className="flex-1 flex justify-center">
           <div className="max-w-[445px] w-full flex flex-col gap-8">
-            <span className="uppercase text-orange-500 text-sm font-bold">
+            <span className="uppercase text-orange-500 text-sm font-bold tracking-widest">
               sneaker company
             </span>
 
-            <h1 className="text-5xl font-bold">
+            <h1 className="text-5xl font-bold transition-colors">
               Fall Limited Edition Sneakers
             </h1>
 
-            <p className="text-grayish_blue-500">
+            <p className="text-grayish_blue-500 dark:text-grayish_blue-400 transition-colors">
               These low-profile sneakers are you perfect casual wear companion.
               Featuring a durable rubber outer sole, they'll withstand
               everything the weather can offer.
             </p>
 
             <div>
-              <div>
-                <strong>$125.00</strong>
-                <span>50%</span>
+              <div className="flex items-center gap-3 transition-colors">
+                <strong className="text-2xl">$125.00</strong>
+                <span className="bg-orange-100 rounded px-2 text-sm text-orange-500 font-bold dark:bg-orange-700 dark:text-orange-200 transition-colors">
+                  50%
+                </span>
               </div>
-              <span>$250.00</span>
+              <del className="text-grayish_blue-400 dark:text-grayish_blue-500 transition-colors">
+                $250.00
+              </del>
             </div>
 
             <div className="flex items-center gap-4">
-              <div className="flex items-center justify-between text-orange-500 px-4 h-14 bg-grayish_blue-100 w-[157px] rounded-xl">
+              <div className="flex items-center justify-between text-orange-500 px-4 h-14 bg-grayish_blue-100 w-[157px] rounded-xl dark:bg-grayish_blue-700 transition-colors">
                 <button onClick={() => setQtd(qtd - 1)}>
                   <Minus size={16} weight="bold" />
                 </button>
-                <span className="text-grayish_blue-800">{qtd}</span>
+                <span className="text-grayish_blue-800 dark:text-grayish_blue-100 font-bold">
+                  {qtd}
+                </span>
                 <button onClick={() => setQtd(qtd + 1)}>
                   <Plus size={16} weight="bold" />
                 </button>
