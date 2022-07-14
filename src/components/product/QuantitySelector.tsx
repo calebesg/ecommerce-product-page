72b46c32da012200b919e7bf71222b3aa049aeac
CalebeSG <calebeguimaraes10@gmail.com>
2022-07-14 14:19:44 -0300

    style: opacity effect int the QuantitySelector

--- src/components/product/QuantitySelector.tsx
+++ src/components/product/QuantitySelector.tsx
@@ -17,13 +17,19 @@ export default function QuantitySelector(props: QuantitySelectorProps) {
 
   return (
     <div className="px-4 h-14 w-full lg:w-[157px] rounded-xl flex items-center justify-between text-orange-500 bg-grayish_blue-100 dark:bg-grayish_blue-700 transition-colors">
-      <button onClick={minus}>
+      <button
+        className="active:opacity-0 transition-opacity duration-100"
+        onClick={minus}
+      >
         <Minus size={16} weight="bold" aria-label="remove to cart" />
       </button>
       <span className="font-bold text-grayish_blue-800 dark:text-grayish_blue-100">
         {props.quantity}
       </span>
-      <button onClick={add}>
+      <button
+        className="active:opacity-0 transition-opacity duration-100"
+        onClick={add}
+      >
         <Plus size={16} weight="bold" aria-label="Add to cart" />
       </button>
     </div>
